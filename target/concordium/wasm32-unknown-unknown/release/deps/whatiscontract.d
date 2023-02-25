ca5a84d3ee81c02f0463aa17baa3d83c680c9fc8
somberpaella1 <bluedude444@gmail.com>
2023-02-26 00:59:05 +0700

    finish

---
+++ target/concordium/wasm32-unknown-unknown/release/deps/whatiscontract.d
@@ -0,0 +1,7 @@
+/root/submission/whatiscontract/target/concordium/wasm32-unknown-unknown/release/deps/whatiscontract.wasm: src/lib.rs
+
+/root/submission/whatiscontract/target/concordium/wasm32-unknown-unknown/release/deps/libwhatiscontract.rlib: src/lib.rs
+
+/root/submission/whatiscontract/target/concordium/wasm32-unknown-unknown/release/deps/whatiscontract.d: src/lib.rs
+
+src/lib.rs:
---
+++ target/concordium/wasm32-unknown-unknown/release/libwhatiscontract.d
@@ -0,0 +1,1 @@
+/root/submission/whatiscontract/target/concordium/wasm32-unknown-unknown/release/libwhatiscontract.rlib: /root/submission/whatiscontract/src/lib.rs
